4bca8a0e4d7e64a0c50bbb9b9bd1c1d356523e35
hudsoncsmith <157823509+hudsoncsmith@users.noreply.github.com>
2024-01-30 23:40:49 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
+module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
+}
+
 module.exports = withNextra()
